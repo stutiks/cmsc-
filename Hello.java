6e8f70678db139d6a9003907f779654423b544b0
stutiks <69918523+stutiks@users.noreply.github.com>
2020-08-19 14:31:21 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+	public static void main (String[] args) { 
+	System. out. println ("Stuti Shah");
+	System. out. println ("Hello.java");
+	System. out. println ("Lab 1");
+	System. out. println ("08/19/2020");
+	System. out. println ("CMSC 255");
+	System. out. println ();
+}
+}
+
+
